d10aaf1c13abaf1ccb29143cd083c637b5b98ffd
eldahl <71466904+eldahl@users.noreply.github.com>
2025-10-31 14:33:42 +0100

    antialiasing

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -22,7 +23,16 @@ inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
 }
 inline double radians_to_degrees(double radians) {
-	return radians * 180.0 / pi;
+  return radians * 180.0 / pi;
+}
+inline double random_double() {
+  // Returns a random real in [0,1).
+  return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max - min) * random_double();
 }
 
 // Common Headers
